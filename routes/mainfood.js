639b2210367956270dd99051ae99ad5ea02db7dd
Jackie Lin <jlonf@hotmail.com>
2017-04-30 23:31:36 +0000

    add new route and new form page. add create route and one blog post. Will use past tense for new commit messages in the future

--- routes/mainfood.js
+++ routes/mainfood.js
@@ -15,6 +15,49 @@ router.get("/", function(req, res) {
    
 });
 
+//new route
+router.get("/new",  function(req,res){
+    res.render("foods/newPost.ejs");
+});
+
+//create route
+router.post("/",  function (req, res){
+ 
+   //get data from form and add to array
+   var name = req.body.name;
+   var image = req.body.image;
+   var price = req.body.price;
+   var desc = req.body.description;
+//   var author = {
+//       id: req.user._id,
+//       username:req.user.username
+//   };
+
+//remember to add -> author:author in future versions
+   var newFoodPost= {name: name, price:price, image: image, description: desc};
+   //create a new campground and save to DB
+   foodBlog.create(newFoodPost, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       }   else {
+           console.log(newlyCreated);
+           //redirect back to index foods page
+           res.redirect("/foods");
+       }
+   });
+   
+   });
+
+
+
+
+
+
+
+
+
+
+
 
 
 router.get("*", function(req, res){
